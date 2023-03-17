5f4fac6c70247c9f616c211bdfbcb63e1c11f4c5
Skiff3 <sakibbag@gmail.com>
2023-03-17 13:32:04 +0900

    Rejecting invalid subscribers.

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-//use ZeroToProd::run;
-use tracing_log::LogTracer;
-
-use zerotoprod::startup::run;
-use sqlx::postgres::{PgPool, PgPoolOptions};
-use std::net::TcpListener;
-use zerotoprod::email_client::EmailClient;
-use secrecy::ExposeSecret;
-//use env_logger::Env;
-use zerotoprod::telemetry::{get_subscriber, init_subscriber};
-use tracing::Subscriber;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zerotoprod::configuration::get_configuration;
-//use zerotoprodero::configuration::get_configuration;
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+use zerotoprod::startup::Application;
+use zerotoprod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    //env_logger::init();
-    let subscriber = get_subscriber("ZeroToProd".into(),"info".into(),std::io::stdout);
+async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-    //LogTracer::init().expect("Failed to set logger");
-    // let env_filter = EnvFilter::try_from_default_env()
-    //     .unwrap_or(EnvFilter::new("info"));
-    // let formatting_layer = BunyanFormattingLayer::new(
-    //     "ZeroToProd".into(),
-    //     std::io::stdout
-    // );
-    // let subscriber = Registry::default()
-    //     .with(env_filter)
-    //     .with(JsonStorageLayer)
-    //     .with(formatting_layer);
-    // set_global_default(subscriber).expect("Failed to set subscriber");
-    //env_logger::Builder::from_env(Env::default().default_filter_or("info"));
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new().acquire_timeout(std::time::Duration::from_secs(2)).connect_lazy_with(
-        configuration.database.with_db()//configuration
-    );
-        //.await
-        //.expect("Failed to connect to Postgres.");
-    let sender_email = configuration.email_client.sender()
-        .expect("Invalid sender email address");
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-    );
-    let address = format!("{}:{}",configuration.application.host,configuration.application.port);
-    let listener = TcpListener::bind(address)?;//TcpListener
-    //Ok(())
-    //println!("in main");
-    run(listener,connection_pool,email_client)?.await?;
-    Ok(())//await, connection_pool
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
-
-// pub fn get_subscriber(
-//     name: String,
-//     env_filter: String
-// ) -> impl Subscriber + Send + Sync {
-//     let env_filter = EnvFilter::try_form_default_env()
-//         .unwrap_or_else(|_| EnvFilter::new(env_filter));
-//     let formatting_layer = BunyanFormattingLayer::new(
-//         name,
-//         std::io::stdout
-//     );
-//     Registry::default()
-//         .with(env_filter)
-//         .with(JsonStorageLayer)
-//         .with(formatting_layer)
-// }
-//
-// pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync){
-//     LogTracer::init().expect("Failed to set logger");
-//     set_global_default(subscriber).expect("Failed to set subscriber");
-// }
-// #[tokio::main]
-// async fn main() -> Result<(), std::io::Error> {
-//     HttpServer::new(|| {
-//         App::new()
-//         .route("/", web::get().to(greet))
-//         .route("/{name}", web::get().to(greet))
-//
-//     })
-//     .bind("127.0.0.1:8000")?
-//     .run()
-//     .await
-//}
-// async fn health_check(req: HttpRequest) -> impl
-// Responder {
-//     HttpResponse::Ok()
-// }
-
-// #[tokio::main]
-// async fn main() -> Result<(), std::io::Error> {
-//     HttpServer::new(|| {
-//         App::new()
-//             .route("/health_check",
-//                    web::get().to(health_check))
-//     })
-//         .bind("127.0.0.1:8000")?
-//         .run()
-//         .await
-// }
-//pub fn set_logger(logger: &'static dyn Log) -> Result<(), SetLoggerError>
-
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,158 +1,166 @@
-use actix_web::{HttpResponse, web};
-use sqlx::{PgConnection, PgPool};
-use tracing::{error, Instrument};
-use sqlx::types::uuid;
-use chrono::Utc;
-use unicode_segmentation::UnicodeSegmentation;
-use uuid::Uuid;//---->??
-//use unicoe_segmentation::UnicodeSegmentation;
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
-
+use crate::email_client::EmailClient;
+use crate::startup::ApplicationBaseUrl;
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use sqlx::{PgPool, Postgres, Transaction};
+use std::convert::{TryFrom, TryInto};
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
-    email: String,// this is email of form data.
-    name: String
+    email: String,
+    name: String,
 }
 
-pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber,String>{
-    let name = SubscriberName::parse(form.name)?;
-    let email = SubscriberEmail::parse(form.email)?;
-    Ok(NewSubscriber{email,name})
-}
-
-#[tracing::instrument(
-    name = "Adding a new subscriber",
-    skip(form,pool),
-    fields(
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    )
-)]
-pub async fn subscribe(form: web::Form<FormData>,pool:web::Data<PgPool>,) -> HttpResponse {
-    let new_subscriber = match form.0.try_into() {
-        //Ok(subscriber) => subscriber,
-        Ok(form) => form,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    // let name = match SubscriberName::parse(form.0.name) {
-    //     Ok(name) => name,
-    //     Err(_) => return
-    //     HttpResponse::BadRequest().finish(),
-    // };
-    // let email = match SubscriberEmail::parse(form.0.email){
-    //     Ok(email) => email,
-    //     Err(_) => return
-    //     HttpResponse::BadRequest().finish(),
-    // };
-    // //let new_subscriber = crate::domain::SubscriberName(form.name.clone());
-    // let new_subscriber = NewSubscriber{
-    //     email,
-    //     name,
-    // };
-    // if !is_valid_name(&form.name) {
-    //     return HttpResponse::BadRequest().finish()
-    // }
-
-    match insert_subscriber(&pool, &new_subscriber).await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }//there
-}
-
-pub fn is_valid_name(s: &str) -> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-    let is_too_long = s.graphemes(true).count() > 256;
-    let forbidden_characters = ['/','(',')','"','<','>','\\','{','}'];
-    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
-    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
-}//rends here
-
-#[tracing::instrument(
-    name = "Saving new subscriber details in the database",
-    skip(new_subscriber,pool)
-)]
-pub async fn insert_subscriber(
-    pool: &PgPool,
-    //form: &FormData,
-    new_subscriber: &NewSubscriber,
-) -> Result<(), sqlx::Error> {//the result returns and error
-    sqlx::query!(
-        r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
-        "#,
-        Uuid::new_v4(),
-        new_subscriber.email.as_ref(),
-        new_subscriber.name.as_ref(),//inner_ref()??
-        Utc::now()
-    )
-        .execute(pool)
-        .await
-        .map_err(|e|{
-            tracing::error!("Failed to execute query: {:?}",e);
-        e
-    })?;
-    Ok(())
-}
-
-impl SubscriberName{
-    pub fn inner_ref(&self) -> &str{
-        &self.0
-    }
-}
-
-impl TryFrom<FormData> for NewSubscriber{
+impl TryFrom<FormData> for NewSubscriber {
     type Error = String;
 
     fn try_from(value: FormData) -> Result<Self, Self::Error> {
         let name = SubscriberName::parse(value.name)?;
         let email = SubscriberEmail::parse(value.email)?;
-        Ok(Self{ email, name})
+        Ok(Self { email, name })
     }
 }
-//
-// pub fn do_something_with_a_string_slice<T: AsRef<str>>(s: T){
-//     let s = s.as_ref();
-// }
-//     let request_id = Uuid::new_v4();//Uuid is used to generate a random id
-//     let request_span = tracing::info_span!(
-//         name = "Adding a new subscriber.",//tracing when a new subscriber is added.
-//         skip(form,pool),
-//         fields(
-//             %request_id,  //request_id of the subscriber to track the error better in the log trace
-//             subscriber_email = %form.email, //e-mail of the subscriber in the log trace
-//             subscriber_name = %form.name//name of the subscriber in the log trace
-//         )
-//
-//         // &a-->lj.23999909887,9.99898989,3.45888890,2.788889988=>{
-//         //     &b,&b,&v,&c(0.999,0.88,0.898,0.8876)
-//         //     #90099998.kjjj99999&88
-//         // }
-//     );
-//     let_request_span_guard = request_span.enter();
-//     let query_span = tracing::info_span!("Saving new subscriber details in the database");
-//     match sqlx::query!(
-//         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
-//         Uuid::new_v4(),//Uuid is used to generate random id for the user in the table
-//         form.email,//e-mail of the subscriber in the query
-//         form.name,//name of the subscriber in the query
-//         Utc::now()//timestamp when the query was created.
-//     )
-//         .execute(pool.get_ref())
-//         //First we attach the instrumentation, then we have to wait it out.
-//         .instrument(query_span)
-//         .await
-//     {
-//         Ok(_) => {
-//         //tracing::info!("request_id {} - New subscriber details have been saved",request_id);
-//         HttpResponse::Ok().finish()
-//         },
-//         Err(e) => {
-//             tracing::error!("request_id {} - Failed to execute query: {:?}",request_id,e);//log dependency is used to display errors.
-//             //println!("Failed to execute query: {}",e);
-//             HttpResponse::InternalServerError().finish()
-//         }
-//     }
-// }
+
+#[allow(clippy::async_yields_async)]
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool, email_client, base_url),
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+    base_url: web::Data<ApplicationBaseUrl>,
+) -> HttpResponse {
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+    let mut transaction = match pool.begin().await {
+        Ok(transaction) => transaction,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+    let subscriber_id = match insert_subscriber(&mut transaction, &new_subscriber).await {
+        Ok(subscriber_id) => subscriber_id,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+    let subscription_token = generate_subscription_token();
+    if store_token(&mut transaction, subscriber_id, &subscription_token)
+        .await
+        .is_err()
+    {
+        return HttpResponse::InternalServerError().finish();
+    }
+    if transaction.commit().await.is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
+    if send_confirmation_email(
+        &email_client,
+        new_subscriber,
+        &base_url.0,
+        &subscription_token,
+    )
+    .await
+    .is_err()
+    {
+        return HttpResponse::InternalServerError().finish();
+    }
+    HttpResponse::Ok().finish()
+}
+
+fn generate_subscription_token() -> String {
+    let mut rng = thread_rng();
+    std::iter::repeat_with(|| rng.sample(Alphanumeric))
+        .map(char::from)
+        .take(25)
+        .collect()
+}
+
+#[tracing::instrument(
+    name = "Send a confirmation email to a new subscriber",
+    skip(email_client, new_subscriber, base_url, subscription_token)
+)]
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+    base_url: &str,
+    subscription_token: &str,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = format!(
+        "{}/subscriptions/confirm?subscription_token={}",
+        base_url, subscription_token
+    );
+    let plain_body = format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        confirmation_link
+    );
+    let html_body = format!(
+        "Welcome to our newsletter!<br />Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+    email_client
+        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .await
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(new_subscriber, transaction)
+)]
+pub async fn insert_subscriber(
+    transaction: &mut Transaction<'_, Postgres>,
+    new_subscriber: &NewSubscriber,
+) -> Result<Uuid, sqlx::Error> {
+    let subscriber_id = Uuid::new_v4();
+    sqlx::query!(
+        r#"
+    INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+    VALUES ($1, $2, $3, $4, 'pending_confirmation')
+            "#,
+        subscriber_id,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
+        Utc::now()
+    )
+    .execute(transaction)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(subscriber_id)
+}
+
+#[tracing::instrument(
+    name = "Store subscription token in the database",
+    skip(subscription_token, transaction)
+)]
+pub async fn store_token(
+    transaction: &mut Transaction<'_, Postgres>,
+    subscriber_id: Uuid,
+    subscription_token: &str,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+    INSERT INTO subscription_tokens (subscription_token, subscriber_id)
+    VALUES ($1, $2)
+        "#,
+        subscription_token,
+        subscriber_id
+    )
+    .execute(transaction)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
+}
